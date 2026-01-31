bb05b8f948fdd294e3e2a884975fd87087cd4f0f
Luka Gogichaishvili <luka595664@gmail.com>
2026-01-31 15:09:40 +0400

    init

---
+++ src/main/java/com/Samkarro/CMSapi/CmSapiApplication.java
@@ -0,0 +1,13 @@
+package com.Samkarro.CMSapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmSapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmSapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Samkarro/CMSapi/CmSapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Samkarro.CMSapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmSapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
