2201a8dbab89e329f4a913741f4c2baa5c29fd3e
Luka Gogichaishvili <luka595664@gmail.com>
2026-02-02 18:13:25 +0400

    feat: List categories

--- src/main/java/com/Samkarro/CMSapi/controller/CategoryController.java
+++ src/main/java/com/Samkarro/CMSapi/controller/CategoryController.java
@@ -1,11 +1,28 @@
 package com.Samkarro.CMSapi.controller;
 
+import com.Samkarro.CMSapi.model.Category;
+import com.Samkarro.CMSapi.service.CategoryService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/category")
 public class CategoryController {
+
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service){
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public List<Category> ListCategories(){
+        return this.service.ListCategories();
+    }
+
 }
--- src/main/java/com/Samkarro/CMSapi/service/CategoryService.java
+++ src/main/java/com/Samkarro/CMSapi/service/CategoryService.java
@@ -1,7 +1,21 @@
 package com.Samkarro.CMSapi.service;
 
+import com.Samkarro.CMSapi.model.Category;
+import com.Samkarro.CMSapi.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public CategoryService(CategoryRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Category> ListCategories(){
+        return this.repository.findAll();
+    }
 }
