11967723c3fa58a7c715fac6e8fc3bb7153b7d85
Luka Gogichaishvili <luka595664@gmail.com>
2026-02-02 19:06:28 +0400

    feat: Many to Many relationship btwn/Article & Category

--- src/main/java/com/Samkarro/CMSapi/model/Category.java
+++ src/main/java/com/Samkarro/CMSapi/model/Category.java
@@ -2,34 +2,45 @@ package com.Samkarro.CMSapi.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
-@Table
+@Table(name = "category")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int articleId;
     private String name;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Article> articles = new HashSet<>();
+
+    public Set<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Set<Article> articles) {
+        this.articles = articles;
+    }
 
     @JsonCreator
     public Category(){}
 
-    public Category(int id, String name) {
-        this.id = id;
+    public Category(int articleId, String name) {
+        this.articleId = articleId;
         this.name = name;
     }
 
-    public int getId() {
-        return id;
+    public int getArticleId() {
+        return articleId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
     }
 
     public String getName() {
