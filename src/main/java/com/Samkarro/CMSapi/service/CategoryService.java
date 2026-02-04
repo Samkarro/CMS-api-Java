f20d625fc59ff24cca3b5a7e2d3c67dfe30c2fab
Luka Gogichaishvili <luka595664@gmail.com>
2026-02-04 22:19:27 +0400

    feat: update article

--- src/main/java/com/Samkarro/CMSapi/service/CategoryService.java
+++ src/main/java/com/Samkarro/CMSapi/service/CategoryService.java
@@ -1,9 +1,12 @@
 package com.Samkarro.CMSapi.service;
 
+import com.Samkarro.CMSapi.model.Article;
 import com.Samkarro.CMSapi.model.Category;
+import com.Samkarro.CMSapi.repository.ArticleRepository;
 import com.Samkarro.CMSapi.repository.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -11,25 +14,34 @@ import java.util.List;
 @Service
 public class CategoryService {
 
-    private final CategoryRepository repository;
+    private final CategoryRepository categoryRepo;
+    private final ArticleRepository articleRepo;
 
-    public CategoryService(CategoryRepository repository){
-        this.repository = repository;
+    public CategoryService(CategoryRepository categoryRepo, ArticleRepository articleRepo){
+        this.categoryRepo = categoryRepo;
+        this.articleRepo = articleRepo;
     }
 
     public List<Category> ListCategories(){
-        return this.repository.findAll();
+        return this.categoryRepo.findAll();
     }
 
     public Category Create(Category category) {
-        return this.repository.save(category);
+        return this.categoryRepo.save(category);
     }
 
+    @Transactional
     public void Delete(int id) {
-        var forDeletion = this.repository.findById(id).orElse(null);
-        if(forDeletion == null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with provided id not found");
+        var category = this.categoryRepo.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+        List<Article> articlesWithCategory = this.articleRepo.findAllByCategories(category);
+
+        for (Article article : articlesWithCategory) {
+            article.getCategories().remove(category);
         }
-        this.repository.deleteById(id);
+
+        this.articleRepo.saveAll(articlesWithCategory);
+        this.categoryRepo.delete(category);
     }
 }
